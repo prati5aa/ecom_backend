6bf81e0428920b5215ae3189402c2fba9061199c
prati5aa <thapapratik226@gmail.com>
2025-07-10 15:43:05 +0545

    get

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,10 @@ const {createUser, login} = require('../controller/user.controller')
 router.post('/signin',createUser)
 
 router.post('/login',login)
+router.get('/login', (req, res) => {
+ 
+    res.status(200).json({ message: 'hit user/login' });
+});
 
 // router.route('/user/:id').delete().patch()
 
